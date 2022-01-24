f46861bc0c4e8ac46360e4898cf1fecb2f0d71c4
fernandofrantz <fernandofrantz12@gmail.com>
2022-01-24 18:11:17 -0300

    some routes added

--- app/routes/post_route.py
+++ app/routes/post_route.py
@@ -1,5 +1,21 @@
 def post_route(app):
 
+    @app.post("/posts")
+    def create_post():
+        return ''
+
+    @app.delete("/posts/<int:id>")
+    def delete_post():
+        return ''
+
+    @app.get("/posts/<int:id>")
+    def read_post_by_id():
+        return ''
+
     @app.get("/posts")
     def read_posts():
+        return ''
+
+    @app.patch("/posts/<int:id>")
+    def update_post():
         return ''
